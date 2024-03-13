f2b2a488cf034790691ce9cf9ce83df18f9b2cf5
ishawant kumar <ishawantkumar@gmail.com>
2024-03-13 13:54:11 +0530

    data transformation related constant updated

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -14,6 +14,18 @@ CONFIG_DIR='config'
 SCHEMA_FILE='config.yaml'
 CONFIG_FILE_PATH=os.path.join(ROOT_DIR,CONFIG_DIR,SCHEMA_FILE)
 
+# transformation File path 
+ROOT_DIR=os.getcwd()
+CONFIG_DIR='config'
+TRANSFORMATION_FILE='transformation.yaml'
+TRANFORMATION_YAML_FILE_PATH=os.path.join(ROOT_DIR,CONFIG_DIR,TRANSFORMATION_FILE)
+TARGET_COLUMN_KEY= 'target_column'
+NUMERICAL_COLUMN_KEY= 'numerical_columns'
+CATEGORICAL_COLUMNS ='categorical_columns'
+BOOLEAN_COLUMN = 'boolean_columns'
+DROP_COLUMNS= 'drop_columns'
+SCALING_COLUMNS='scaling_columns'
+
 # Data Ingestion related variable
 DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
 DATA_INGESTION_DATABASE_NAME= "data_base"
@@ -33,3 +45,13 @@ DATA_VALIDATION_SCHEMA_DIR_KEY = "schema_dir"
 DATA_VALIDATION_VALID_DATASET ="validated_data"
 DATA_VALIDATION_TRAIN_FILE = "Train_data"
 DATA_VALIDATION_TEST_FILE ="Test_data"
+
+# Data Transformation related variable 
+DATA_TRANSFORMATION_CONFIG_KEY = "data_transformation_config"
+DATA_TRANSFORMATION='data_transformation_dir'
+DATA_TRANSFORMATION_DIR_NAME_KEY = "transformed_dir"
+DATA_TRANSFORMATION_TRAIN_DIR_NAME_KEY = "transformed_train_dir"
+DATA_TRANSFORMATION_TEST_DIR_NAME_KEY = "transformed_test_dir"
+DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY = "preprocessing_dir"
+DATA_TRANSFORMATION_PREPROCESSING_FILE_NAME_KEY = "preprocessed_object_file_name"
+DATA_TRANSFORMATION_FEA_ENG_FILE_NAME_KEY='feature_eng_file'
